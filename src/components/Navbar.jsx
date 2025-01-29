8b080125bb1eb092c782316bc071f13654b19858
Gowsalya-Gopalakrishnan <gowsalyagopalakrishnan1515@gmail.com>
2025-01-29 21:47:24 +0530

    files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Home from './Home'
 import Dashboard from './Dashboard'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <>
-    <Link to='/home' element={<Home/>}></Link>
-    <Link to='/about' element={<Dashboard/>}></Link>
+    <Link to='/home' element={<Home/>}>Home</Link>
+    <Link to='/about' element={<Dashboard/>}>Dasboard</Link>
     </>
     
   )

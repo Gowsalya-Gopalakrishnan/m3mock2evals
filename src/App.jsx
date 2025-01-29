8be7b6ef7bb434d2a880ff6085e404024b7e712b
Gowsalya-Gopalakrishnan <gowsalyagopalakrishnan1515@gmail.com>
2025-01-29 21:32:12 +0530

    files

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 // import Navbr from './components/Navbar'
 import Navbar from './components/Navbar'
-import { Route , Routes} from 'react-router-dom'
-import Home from './Pages/Home'
-import Dashboard from './Pages/Dashboard'
-import Login from './Pages/Login'
-import PrivateRoute from './Pages/PrivateRoute'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <Navbar/>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './Home'
+import Dashboard from './Dashboard'
+
+const Navbar = () => {
+  return (
+    <>
+    <Home/>
+    <Dashboard/>
+    </>
+    
+  )
+}
+
+export default Navbar

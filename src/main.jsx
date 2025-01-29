64748e1017cd0abc06e08619ecd5819c9f131b6e
Gowsalya-Gopalakrishnan <gowsalyagopalakrishnan1515@gmail.com>
2025-01-29 21:36:55 +0530

    files

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
-import ContextProvider from './Context/ContextProvider.jsx'
+// import ContextProvider from './Context/ContextProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
